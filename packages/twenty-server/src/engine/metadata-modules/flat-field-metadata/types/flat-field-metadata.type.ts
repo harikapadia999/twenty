0b1b5e637f82a90f71500f1a6875dd303d8d37f7
prastoin <paul@twenty.com>
2025-12-09 17:58:16 +0100

    lint

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type.ts
@@ -1,7 +1,7 @@
 import { type FieldMetadataType } from 'twenty-shared/types';
 
 import { type FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
-import { FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
+import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
 
 export const FIELD_METADATA_RELATION_PROPERTIES = [
   'relationTargetFieldMetadata',
--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/types/extract-record-typeorm-nullable-date-properties.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/types/extract-record-typeorm-nullable-date-properties.type.ts
@@ -1,6 +1,6 @@
 export type ExtractRecordTypeOrmNullableDateProperties<T> = NonNullable<
   {
-    [P in keyof T]:  T[P] extends null
+    [P in keyof T]: T[P] extends null
       ? [NonNullable<T[P]>] extends [never]
         ? never
         : NonNullable<T[P]> extends Date
