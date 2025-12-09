892c4827752d478ed55e2eec67dca3442a1e2509
prastoin <paul@twenty.com>
2025-12-09 19:09:20 +0100

    feat(server): simplify typing

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity.type.ts
@@ -1,10 +1,7 @@
 import { type SyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface';
 
-import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
-import { type ExtractRecordTypeOrmNonNullableDateProperties } from 'src/engine/workspace-manager/workspace-migration-v2/types/extract-record-typeorm-non-nullable-date-properties.type';
-import { type ExtractRecordTypeOrmNullableDateProperties } from 'src/engine/workspace-manager/workspace-migration-v2/types/extract-record-typeorm-nullable-date-properties.type';
+import { CastRecordTypeOrmDatePropertiesToString } from 'src/engine/workspace-manager/workspace-migration-v2/types/cast-record-typeorm-date-properties-to-string.type';
 import { type NonNullableProperties } from 'src/types/non-nullable-properties.type';
-import { FieldMetadataType } from 'twenty-shared/types';
 
 export type SyncableFlatEntity = NonNullableProperties<
   Omit<SyncableEntity, 'application' | 'applicationId'>
@@ -13,30 +10,13 @@ export type SyncableFlatEntity = NonNullableProperties<
   applicationId: string | null;
 };
 
-export type EntityNullableDateProperties<TEntity> =
-  ExtractRecordTypeOrmNullableDateProperties<TEntity>;
-
-export type EntityNonNullableDateProperties<TEntity> =
-  ExtractRecordTypeOrmNonNullableDateProperties<TEntity>;
-
 export type FlatEntityFrom<
   TEntity,
   TEntityRelationProperties extends keyof TEntity,
-> = {
-  [P in EntityNonNullableDateProperties<TEntity>]: string;
-} & {
-  [P in EntityNullableDateProperties<TEntity>]: string | null;
-} & Omit<
-    TEntity,
-    | TEntityRelationProperties
-    | EntityNonNullableDateProperties<TEntity>
-    | EntityNullableDateProperties<TEntity>
-    | 'application'
-  >;
-
-type toto = EntityNonNullableDateProperties<
-  FieldMetadataEntity<FieldMetadataType.SELECT>
->;
-type toto2 = EntityNullableDateProperties<
-  FieldMetadataEntity<FieldMetadataType.SELECT>
->;
+> = Omit<
+  TEntity,
+  | TEntityRelationProperties
+  | 'application'
+  | keyof CastRecordTypeOrmDatePropertiesToString<TEntity>
+> &
+  CastRecordTypeOrmDatePropertiesToString<TEntity>;
